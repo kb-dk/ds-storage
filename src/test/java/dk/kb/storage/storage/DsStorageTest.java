16bcfde654e6c18735c3e35c79c3ea7d766bf4b5
Thomas Egense <teg@kb.dk>
2021-11-24 10:17:40 +0100

    Extracted all unittest setup (database+load properties) to an abstract class. Other unittest classes can extend this class and get database setup+properties loaded .

--- src/test/java/dk/kb/storage/storage/DsStorageTest.java
+++ src/test/java/dk/kb/storage/storage/DsStorageTest.java
@@ -3,13 +3,6 @@ package dk.kb.storage.storage;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -27,92 +20,16 @@ import dk.kb.storage.util.UniqueTimestampGenerator;
 
 
 
-public class DsStorageTest {
+public class DsStorageTest extends DsStorageUnitTestUtil{
 
 	private static final Logger log = LoggerFactory.getLogger(DsStorageTest.class);
-	private static final String CREATE_TABLES_DDL_FILE = "ddl/create_ds_storage.ddl";
-
-	private static final String DRIVER = "org.h2.Driver";
-
-	//We need the relative location. This works both in IDE's and Maven.
-	private static final String TEST_CLASSES_PATH = new File(Thread.currentThread().getContextClassLoader().getResource("logback-test.xml").getPath()).getParentFile().getAbsolutePath();
-	private static final String URL = "jdbc:h2:"+TEST_CLASSES_PATH+"/h2/ds_storage;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE";
-	private static final String USERNAME = "";
-	private static final String PASSWORD = "";
-
-	private static DsStorage storage = null;
-
-
-	private static void createEmptyDBFromDDL() throws Exception {
-		// Delete if exists
-		doDelete(new File(TEST_CLASSES_PATH +"/h2"));
-		try {
-			Class.forName(DRIVER); // load the driver
-		} catch (ClassNotFoundException e) {
-
-			throw new SQLException(e);
-		}
-
-
-		try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)){
-			File file = getFile(CREATE_TABLES_DDL_FILE);
-			log.info("Running DDL script:" + file.getAbsolutePath());
-
-			if (!file.exists()) {
-				log.error("DDL script not found:" + file.getAbsolutePath());
-				throw new RuntimeException("DDL Script file not found:" + file.getAbsolutePath());
-			}
-
-			connection.createStatement().execute("RUNSCRIPT FROM '" + file.getAbsolutePath() + "'");
-
-			connection.createStatement().execute("SHUTDOWN");
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
-
-	@BeforeAll
-	public static void beforeClass() throws Exception {
-		ServiceConfig.initialize("conf/ds-storage*.yaml"); 	    
-		createEmptyDBFromDDL();
-		DsStorage.initialize(DRIVER, URL, USERNAME, PASSWORD);
-		storage = new  DsStorage();
-
-
-	}
-	@BeforeEach
-	public void beforeEach() throws Exception {	        	    		    	
-		storage.rollback(); //Important so each unittest has clean table
-	}
 
 
 
-	@AfterAll
-	public static void afterClass() {
-		// No reason to delete DB data after test, since we delete it before each test.
-		// This way you can open the DB in a DB-browser after a unittest and see the result.
-		DsStorage.shutdown();
-	}
-
-
-	// file.delete does not work for a directory unless it is empty. hence this method
-	protected static void doDelete(File path) {
-		if (path.isDirectory()) {
-			for (File child : path.listFiles()) {
-				doDelete(child);
-			}
-		}
-		if (!path.delete()) {
-			log.info("Could not delete " + path);
-		}
-	}
 
 
 	@Test
-	public void testBAsicCRUD() throws Exception {
+	public void testBasicCRUD() throws Exception {
      //TODO rescribe flow below
 		
         //Test record not exist
@@ -148,7 +65,6 @@ public class DsStorageTest {
 
 		String dataUpdate = "Hello updated";
 		String parentIdUpdated="id_2_parent";
-		long mTimeBefore = recordLoaded.getmTime(); // Must be newer after update
 		long cTimeBefore = recordLoaded.getcTime(); //Must be the same
 
 		record.setDeleted(true);
@@ -316,27 +232,5 @@ public class DsStorageTest {
 		assertEquals(1,baseStatictics.get("test_base3"));	        	     
 	}
 
-	//TODO TOES? Is this somewhere in kb-util ?
-	/**
-	 * Multi protocol resource loader. Primary attempt is direct file, secondary is classpath resolved to File.
-	 *
-	 * @param resource a generic resource.
-	 * @return a File pointing to the resource.
-	 */
-	protected static File getFile(String resource) throws IOException {
-		File directFile = new File(resource);
-		if (directFile.exists()) {
-			return directFile;
-		}
-		URL classLoader = Thread.currentThread().getContextClassLoader().getResource(resource);
-		if (classLoader == null) {
-			throw new FileNotFoundException("Unable to locate '" + resource + "' as direct File or on classpath");
-		}
-		String fromURL = classLoader.getFile();
-		if (fromURL == null || fromURL.isEmpty()) {
-			throw new FileNotFoundException("Unable to convert URL '" + fromURL + "' to File");
-		}
-		return new File(fromURL);
-	}
 
 }
---
+++ src/test/java/dk/kb/storage/storage/DsStorageUnitTestUtil.java
@@ -0,0 +1,137 @@
+package dk.kb.storage.storage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dk.kb.storage.config.ServiceConfig;
+
+
+/*
+ * Setup for the environment for unittest the same way as done in the InitialContext loader in the web container.
+ * 
+ * 1) Create a h2 database for unittests with schema defined
+ * 2) Load the Yaml property files.
+ * 
+ */
+public abstract class DsStorageUnitTestUtil {
+
+	protected static final String CREATE_TABLES_DDL_FILE = "ddl/create_ds_storage.ddl";
+
+	protected static final String DRIVER = "org.h2.Driver";
+
+	//We need the relative location. This works both in IDE's and Maven.
+	protected static final String TEST_CLASSES_PATH = new File(Thread.currentThread().getContextClassLoader().getResource("logback-test.xml").getPath()).getParentFile().getAbsolutePath();
+	protected static final String URL = "jdbc:h2:"+TEST_CLASSES_PATH+"/h2/ds_storage;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE";
+	protected static final String USERNAME = "";
+	protected static final String PASSWORD = "";
+
+	protected static DsStorage storage = null;
+
+	private static final Logger log = LoggerFactory.getLogger(DsStorageUnitTestUtil.class);
+
+
+	@BeforeAll
+	public static void beforeClass() throws Exception {
+		ServiceConfig.initialize("conf/ds-storage*.yaml"); 	    
+		createEmptyDBFromDDL();
+		DsStorage.initialize(DRIVER, URL, USERNAME, PASSWORD);
+		storage = new DsStorage();
+
+
+	}
+	@BeforeEach
+	public void beforeEach() throws Exception {	        	    		    	
+		storage.rollback(); //Important so each unittest has clean table
+	}
+
+
+
+	@AfterAll
+	public static void afterClass() {
+		// No reason to delete DB data after test, since we delete it before each test.
+		// This way you can open the DB in a DB-browser after a unittest and see the result.
+		DsStorage.shutdown();
+	}
+
+	
+	
+	protected static void createEmptyDBFromDDL() throws Exception {
+		// Delete if exists
+		doDelete(new File(TEST_CLASSES_PATH +"/h2"));
+		try {
+			Class.forName(DRIVER); // load the driver
+		} catch (ClassNotFoundException e) {
+
+			throw new SQLException(e);
+		}
+
+		try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)){
+			File file = getFile(CREATE_TABLES_DDL_FILE);
+			log.info("Running DDL script:" + file.getAbsolutePath());
+
+			if (!file.exists()) {
+				log.error("DDL script not found:" + file.getAbsolutePath());
+				throw new RuntimeException("DDL Script file not found:" + file.getAbsolutePath());
+			}
+
+			connection.createStatement().execute("RUNSCRIPT FROM '" + file.getAbsolutePath() + "'");
+			connection.createStatement().execute("SHUTDOWN");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	//TODO TOES? Is this somewhere in kb-util ?
+	/**
+	 * Multi protocol resource loader. Primary attempt is direct file, secondary is classpath resolved to File.
+	 *
+	 * @param resource a generic resource.
+	 * @return a File pointing to the resource.
+	 */
+	protected static File getFile(String resource) throws IOException {
+		File directFile = new File(resource);
+		if (directFile.exists()) {
+			return directFile;
+		}
+		URL classLoader = Thread.currentThread().getContextClassLoader().getResource(resource);
+		if (classLoader == null) {
+			throw new FileNotFoundException("Unable to locate '" + resource + "' as direct File or on classpath");
+		}
+		String fromURL = classLoader.getFile();
+		if (fromURL == null || fromURL.isEmpty()) {
+			throw new FileNotFoundException("Unable to convert URL '" + fromURL + "' to File");
+		}
+		return new File(fromURL);
+	}
+
+	
+	
+	// file.delete does not work for a directory unless it is empty. hence this method
+	protected static void doDelete(File path) {
+		if (path.isDirectory()) {
+			for (File child : path.listFiles()) {
+				doDelete(child);
+			}
+		}
+		if (!path.delete()) {
+			log.info("Could not delete " + path);
+		}
+	}
+
+
+	
+	
+}
