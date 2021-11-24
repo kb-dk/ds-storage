b2754340c30ae0414cb8fee853de8d4bb9819bf1
Thomas Egense <teg@kb.dk>
2021-11-24 12:01:58 +0100

    cleanup imports

--- src/test/java/dk/kb/storage/config/facade/DsStorageFacadeTest.java
+++ src/test/java/dk/kb/storage/config/facade/DsStorageFacadeTest.java
@@ -2,8 +2,6 @@ package dk.kb.storage.config.facade;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
