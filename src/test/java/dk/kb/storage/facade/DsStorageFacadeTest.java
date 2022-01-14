c04eb077e2f754545d8243bfbdae31b063f03c33
Thomas Egense <teg@kb.dk>
2022-01-14 10:46:06 +0100

    OAIHarvestClient test class created

--- src/test/java/dk/kb/storage/facade/DsStorageFacadeTest.java
+++ src/test/java/dk/kb/storage/facade/DsStorageFacadeTest.java
@@ -29,29 +29,6 @@ import dk.kb.storage.util.OaiResponse;
 
 public class DsStorageFacadeTest extends DsStorageUnitTestUtil{
 
-   
-    
-    @Test
-    public void testOAI() throws Exception {
-   
-       String uri="http://www5.kb.dk/cop/oai/?metadataPrefix=mods&set=oai:kb.dk:images:billed:2010:okt:billeder&verb=";
-       String baseUrl="http://www5.kb.dk/cop/oai/";
-       String metadataPrefix="mods";        
-       String verb="ListRecords";
-       String set="oai:kb.dk:images:billed:2010:okt:billeder";
-        
-        OaiResponse oaiResponse = OaiHarvestClient.harvestOAI(baseUrl, metadataPrefix, set, verb);
-        System.out.println(oaiResponse.getRecords().size());
-        System.out.println(oaiResponse.getResumptionToken());
-        
-        OaiRecord r = oaiResponse.getRecords().get(0);
-        System.out.println(r.getId());
-        System.out.println(r.getMetadata());         
-        }
-      
-   
-
-
     @Test
     public void testCreateAndUpdate() throws Exception {
         //TODO rescribe flow below
---
+++ src/test/java/dk/kb/storage/util/OaiHarvestClientTest.java
@@ -0,0 +1,33 @@
+package dk.kb.storage.util;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * This is an integration test that must be run manually.
+ * 
+ */
+public class OaiHarvestClientTest {
+
+    
+/*     
+    @Test
+    public void testOAI() throws Exception {
+   
+       String uri="http://www5.kb.dk/cop/oai/?metadataPrefix=mods&set=oai:kb.dk:images:billed:2010:okt:billeder&verb=";
+       String baseUrl="http://www5.kb.dk/cop/oai/";
+       String metadataPrefix="mods";        
+       String verb="ListRecords";
+       String set="oai:kb.dk:images:billed:2010:okt:billeder";
+        
+        OaiResponse oaiResponse = OaiHarvestClient.harvestOAI(baseUrl, metadataPrefix, set, verb);
+        System.out.println(oaiResponse.getRecords().size());
+        System.out.println(oaiResponse.getResumptionToken());
+        
+        OaiRecord r = oaiResponse.getRecords().get(0);
+        System.out.println(r.getId());
+        System.out.println(r.getMetadata());         
+        }
+      
+   */
+
+}
