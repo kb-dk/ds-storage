40636e7bde777bf3256d1c6a252c349e9a6c0180
Thomas Egense <teg@kb.dk>
2022-01-31 14:27:09 +0100

    Switched to kb-util 1.4.6 and using the Resolver class to find resources on classpath

--- src/test/java/dk/kb/storage/storage/DsStorageUnitTestUtil.java
+++ src/test/java/dk/kb/storage/storage/DsStorageUnitTestUtil.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import dk.kb.storage.config.ServiceConfig;
+import dk.kb.util.Resolver;
 
 
 /*
@@ -103,29 +105,11 @@ public abstract class DsStorageUnitTestUtil {
 
     }
 
-    //TODO TOES? Is this somewhere in kb-util ?
-    /**
-     * Multi protocol resource loader. Primary attempt is direct file, secondary is classpath resolved to File.
-     *
-     * @param resource a generic resource.
-     * @return a File pointing to the resource.
-     */
+    
+    //Use KB-util to resolve file. 
     protected static File getFile(String resource) throws IOException {
-        File directFile = new File(resource);
-        if (directFile.exists()) {
-            return directFile;
-        }
-        URL classLoader = Thread.currentThread().getContextClassLoader().getResource(resource);
-        if (classLoader == null) {
-            throw new FileNotFoundException("Unable to locate '" + resource + "' as direct File or on classpath");
-        }
-        String fromURL = classLoader.getFile();
-        if (fromURL == null || fromURL.isEmpty()) {
-            throw new FileNotFoundException("Unable to convert URL '" + fromURL + "' to File");
-        }
-        return new File(fromURL);
+       return Resolver.getPathFromClasspath(resource).toFile(); 
     }
-
 
 /*
     // file.delete does not work for a directory unless it is empty. hence this method
