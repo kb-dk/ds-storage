2882f7b4dee34b6e98d5a9cc1d72c7ae732887a4
Toke Eskildsen <toes@kb.dk>
2023-03-06 12:15:52 +0100

    Remove redundant files

--- src/main/java/dk/kb/storage/webservice/Application_v1.java
+++ src/main/java/dk/kb/storage/webservice/Application_v1.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import dk.kb.storage.api.v1.impl.DsStorageApiServiceImpl;
 import dk.kb.storage.api.v1.impl.ServiceApiServiceImpl;
+import dk.kb.util.webservice.exception.ServiceExceptionMapper;
 
 
 public class Application_v1 extends javax.ws.rs.core.Application {
