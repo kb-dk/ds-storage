ec68a16f636864bf40dbfee2c40710c866a997c8
Thomas Egense <teg@kb.dk>
2023-10-10 14:50:29 +0200

    Unittest cleanup

--- src/test/java/dk/kb/storage/storage/DsStorageUnitTestUtil.java
+++ src/test/java/dk/kb/storage/storage/DsStorageUnitTestUtil.java
@@ -1,13 +1,6 @@
 package dk.kb.storage.storage;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Path;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -17,7 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import dk.kb.storage.config.ServiceConfig;
 import dk.kb.storage.util.H2DbUtil;
-import dk.kb.util.Resolver;
+
 
 
 /*
