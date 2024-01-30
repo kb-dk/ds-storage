3ad0bb33b1a6188636f5117929c56e8a01ca48c4
Victor Harbo Johnston <vhol@kb.dk>
2024-01-30 12:28:41 +0100

    rename build.properties

--- src/main/java/dk/kb/storage/webservice/ContextListener.java
+++ src/main/java/dk/kb/storage/webservice/ContextListener.java
@@ -55,8 +55,7 @@ public class ContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         //Workaround for logback problem. This should be called before any logging takes place
         initLogging();
-
-    	
+        BuildInfoManager.loadBuildInfo("ds-storage.build.properties");
     	try {
             RuntimeMXBean mxBean = ManagementFactory.getRuntimeMXBean();
             if (mxBean.getInputArguments().stream().noneMatch(arg -> arg.startsWith("-Xmx"))) {
