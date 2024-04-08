a56f4765b0def39c45153a34905e90031c7ba0c7
Victor Harbo Johnston <vhol@kb.dk>
2024-04-08 12:35:40 +0200

    update OpenApiResource

--- src/main/java/dk/kb/storage/webservice/Application_v1.java
+++ src/main/java/dk/kb/storage/webservice/Application_v1.java
@@ -7,13 +7,17 @@ import java.util.Set;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import dk.kb.storage.api.v1.impl.DsStorageApiServiceImpl;
 import dk.kb.storage.api.v1.impl.ServiceApiServiceImpl;
+import dk.kb.util.webservice.OpenApiResource;
 import dk.kb.util.webservice.exception.ServiceExceptionMapper;
+import dk.kb.storage.config.ServiceConfig;
 
 
 public class Application_v1 extends javax.ws.rs.core.Application {
 
     @Override
     public Set<Class<?>> getClasses() {
+        OpenApiResource.setConfig(ServiceConfig.getConfig());
+
         return new HashSet<>(Arrays.asList(
                 JacksonJsonProvider.class,
                 DsStorageApiServiceImpl.class,
