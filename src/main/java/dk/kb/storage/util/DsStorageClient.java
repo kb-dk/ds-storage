b5f8d759e491bd99abd57d3eef50c5b78862c157
Thomas Egense <teg@kb.dk>
2025-03-03 13:19:51 +0100

    cleanup imports

--- src/main/java/dk/kb/storage/util/DsStorageClient.java
+++ src/main/java/dk/kb/storage/util/DsStorageClient.java
@@ -32,14 +32,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+
 import org.apache.hc.core5.net.URIBuilder;
 
 /**
